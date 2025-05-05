03b996cc3e508a16bdb49c73d919b14875bd6c23
Yurii Kryvoruchko <django.python4132@gmail.com>
2025-05-06 00:08:50 +0300

    Finished the CrossplatformProgramming's Lb8.

---
+++ CrossplatformProgramming/Lb8/src/main/java/MainApp.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+// Завантаження FXML-файлу
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+// Створення сцени з кореневим елементом
+        Scene scene = new Scene(root, 800, 600); // 800x600 розмір вікна
+// Налаштування заголовку та сцени для основного вікна
+        primaryStage.setTitle("Мій JavaFX додаток");
+        primaryStage.setScene(scene);
+// Відображення вікна (за замовчуванням воно прямокутне)
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
