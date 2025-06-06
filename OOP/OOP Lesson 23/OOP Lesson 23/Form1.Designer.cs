0d10972b74e41f2ab150203fb8e022d8bc089bf0
Yurii Kryvoruchko <django.python4132@gmail.com>
2025-06-06 10:56:57 +0300

    Finished the OOP's Lb23.

---
+++ OOP/OOP Lesson 23/OOP Lesson 23/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace OOP_Lesson_23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.lblA = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(150, 15);
+            this.txtA.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(148, 26);
+            this.txtA.TabIndex = 0;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(13, 51);
+            this.btnDraw.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(150, 35);
+            this.btnDraw.TabIndex = 2;
+            this.btnDraw.Text = "Draw Graph";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.BtnDraw_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(13, 96);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1139, 707);
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox_Paint);
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(15, 20);
+            this.lblA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(102, 20);
+            this.lblA.TabIndex = 4;
+            this.lblA.Text = "Coefficient a:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1176, 820);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.txtA);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Parametric Curve Graph";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Button btnDraw;
+    }
+}
+
---
+++ OOP/OOP Lesson 23/OOP Lesson 23/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OOP_Lesson_23
+{
+    public partial class Form1 : Form
+    {
+        private float _a = 1;
+
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BtnDraw_Click(object sender, EventArgs e)
+        {
+            if (float.TryParse(txtA.Text, out float inputA))
+            {
+                if (inputA != 0)
+                {
+                    _a = inputA;
+                    pictureBox.Invalidate();
+                }
+                else
+                {
+                    MessageBox.Show("Coefficients a must be non-zero.", "Invalid Input");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid numbers for a.", "Invalid Input");
+            }
+        }
+
+        private void PictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            int width = pictureBox.Width;
+            int height = pictureBox.Height;
+            int centerX = width / 2;
+            int centerY = height / 2;
+            float scale = 50f;
+
+            using (Pen axisPen = new Pen(Color.Black, 2))
+            {
+                g.DrawLine(axisPen, 0, centerY, width, centerY);
+                g.DrawLine(axisPen, centerX, 0, centerX, height);
+            }
+
+            using (Font font = new Font("Times New Roman", 8))
+            using (Brush brush = new SolidBrush(Color.Black))
+            {
+                int xNumbers = (int)(width / scale) / 2;
+                int yNumbers = (int)(height / scale) / 2;
+                for (int i = -xNumbers; i <= xNumbers; i++)
+                {
+                    if (i != 0)
+                    {
+                        float x = centerX + i * scale;
+                        g.DrawLine(Pens.Black, x, centerY - 5, x, centerY + 5);
+                        g.DrawString(i.ToString(), font, brush, x - 5, centerY + 10);
+                    }
+                }
+
+                for (int i = -yNumbers; i <= yNumbers; i++)
+                {
+                    if (i != 0)
+                    {
+                        float y = centerY - i * scale;
+                        g.DrawLine(Pens.Black, centerX - 5, y, centerX + 5, y);
+                        g.DrawString(i.ToString(), font, brush, centerX + 10, y - 5);
+                    }
+                }
+
+                g.DrawString("X", font, brush, width - 20, centerY + 10);
+                g.DrawString("Y", font, brush, centerX + 10, 10);
+            }
+
+            float[,] ranges = { { -70f, -1f }, { -1f, 70f } };
+            float step = 0.01f;
+
+            using (Pen curvePen = new Pen(Color.Blue, 2))
+            {
+                for (int rangeIndex = 0; rangeIndex < 2; rangeIndex++)
+                {
+                    float minT = ranges[rangeIndex, 0];
+                    float maxT = ranges[rangeIndex, 1];
+                    PointF[] points = new PointF[Convert.ToInt32((maxT - minT) / step) + 1];
+                    int index = 0;
+
+                    for (float t = minT; t < maxT; t += step)
+                    {
+                        float denominator = 1 + t * t * t;
+                        if (Math.Abs(denominator) < 0.001f)
+                            continue;
+
+                        float x = (3 * _a * t) / denominator;
+                        float y = (3 * _a * t * t) / denominator;
+
+                        if (Math.Abs(x) <= 10 && Math.Abs(y) <= 5)
+                        {
+                            points[index] = new PointF(centerX + x * scale, centerY - y * scale);
+                            index++;
+                        }
+                    }
+
+                    if (index > 1)
+                    {
+                        PointF[] curvePoints = new PointF[index];
+                        Array.Copy(points, 0, curvePoints, 0, index);
+                        g.DrawCurve(curvePen, curvePoints);
+                    }
+                }
+            }
+        }
+    }
+}
