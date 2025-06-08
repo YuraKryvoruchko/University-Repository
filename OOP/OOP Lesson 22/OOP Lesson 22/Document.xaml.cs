7846afca2fcd21ae902f266cccda7df6efa4b4d5
Yurii Kryvoruchko <django.python4132@gmail.com>
2025-06-08 22:13:27 +0300

    Finished the OOP's Lb22.

---
+++ OOP/OOP Lesson 22/OOP Lesson 22/Document.xaml.cs
@@ -0,0 +1,239 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_Lesson_22
+{
+    /// <summary>
+    /// Interaction logic for Document.xaml
+    /// </summary>
+    public partial class Document : UserControl
+    {
+        public Document(int number)
+        {
+            InitializeComponent();
+        }
+
+        public void SetText(string text)
+        {
+            TextBoxContent.Document.Blocks.Clear();
+            TextBoxContent.Document.Blocks.Add(new Paragraph(new Run(text)));
+        }
+        public void SaveDocument()
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|Rich Text Format (*.rtf)|*.rtf"
+            };
+
+            if (saveFileDialog1.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog1.FileName;
+                TextRange textRange = new TextRange(TextBoxContent.Document.ContentStart, TextBoxContent.Document.ContentEnd);
+
+                using (System.IO.FileStream fileStream = new System.IO.FileStream(filePath, System.IO.FileMode.Create))
+                {
+                    if (filePath.EndsWith(".rtf"))
+                    {
+                        textRange.Save(fileStream, DataFormats.Rtf);
+                    }
+                    else
+                    {
+                        textRange.Save(fileStream, DataFormats.Text);
+                    }
+                }
+            }
+        }
+
+        public void Cut()
+        {
+            TextBoxContent.Cut();
+        }
+
+        public void Copy()
+        {
+            TextBoxContent.Copy();
+        }
+
+        public void Paste()
+        {
+            TextBoxContent.Paste();
+        }
+
+        public void SelectAll()
+        {
+            TextBoxContent.SelectAll();
+        }
+
+        public void InsertImage()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.png;*.jpeg;*.jpg;*.gif)|*.png;*.jpeg;*.jpg;*.gif|All files (*.*)|*.*",
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                BitmapImage bitmap = new BitmapImage(new Uri(filePath));
+
+                Image image = new Image
+                {
+                    Source = bitmap,
+                    Width = bitmap.PixelWidth,
+                    Height = bitmap.PixelHeight
+                };
+
+                TextPointer tp = TextBoxContent.CaretPosition.GetInsertionPosition(LogicalDirection.Forward);
+                InlineUIContainer container = new InlineUIContainer(image, tp);
+
+            }
+        }
+
+        public void ApplyBold()
+        {
+            ApplyOrRemoveBold(FontWeights.Bold);
+        }
+
+        public void RemoveBold()
+        {
+            ApplyOrRemoveBold(FontWeights.Normal);
+        }
+
+        private void ApplyOrRemoveBold(FontWeight fontWeight)
+        {
+            TextRange textRange = new TextRange(TextBoxContent.Selection.Start, TextBoxContent.Selection.End);
+
+            if (textRange.IsEmpty)
+            {
+                TextBoxContent.FontWeight = fontWeight;
+            }
+            else
+            {
+                object currentValue = textRange.GetPropertyValue(TextElement.FontWeightProperty);
+                if (currentValue != null && currentValue.Equals(fontWeight))
+                {
+                    textRange.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Normal);
+                }
+                else
+                {
+                    textRange.ApplyPropertyValue(TextElement.FontWeightProperty, fontWeight);
+                }
+            }
+        }
+
+        public void ApplyItalic()
+        {
+            ApplyOrRemoveItalic(FontStyles.Italic);
+        }
+
+        public void RemoveItalic()
+        {
+            ApplyOrRemoveItalic(FontStyles.Normal);
+        }
+
+        private void ApplyOrRemoveItalic(FontStyle fontStyle)
+        {
+            TextRange textRange = new TextRange(TextBoxContent.Selection.Start, TextBoxContent.Selection.End);
+
+            if (textRange.IsEmpty)
+            {
+                TextBoxContent.FontStyle = fontStyle;
+            }
+            else
+            {
+                object currentValue = textRange.GetPropertyValue(TextElement.FontStyleProperty);
+                if (currentValue != null && currentValue.Equals(fontStyle))
+                {
+                    textRange.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Normal);
+                }
+                else
+                {
+                    textRange.ApplyPropertyValue(TextElement.FontStyleProperty, fontStyle);
+                }
+            }
+        }
+
+        public void ApplyUnderline()
+        {
+            ApplyOrRemoveStyle(Inline.TextDecorationsProperty, TextDecorations.Underline);
+        }
+        public void RemoveUnderline()
+        {
+            ApplyOrRemoveStyle(Inline.TextDecorationsProperty, null);
+        }
+
+        private void ApplyOrRemoveStyle(DependencyProperty property, object value)
+        {
+            TextRange textRange = new TextRange(TextBoxContent.Selection.Start, TextBoxContent.Selection.End);
+            object currentValue = textRange.GetPropertyValue(property);
+
+            if (currentValue != null && currentValue.Equals(value))
+            {
+                textRange.ApplyPropertyValue(property, null);
+            }
+            else
+            {
+                textRange.ApplyPropertyValue(property, value);
+            }
+        }
+
+        public void ApplyFontFamily(FontFamily fontFamily)
+        {
+            if (TextBoxContent.Selection.IsEmpty)
+                return;
+
+            TextRange textRange = new TextRange(TextBoxContent.Selection.Start, TextBoxContent.Selection.End);
+            textRange.ApplyPropertyValue(TextElement.FontFamilyProperty, fontFamily);
+        }
+
+        public void ApplyFontSize(double fontSize)
+        {
+            if (TextBoxContent.Selection.IsEmpty)
+                return;
+
+            TextRange textRange = new TextRange(TextBoxContent.Selection.Start, TextBoxContent.Selection.End);
+            textRange.ApplyPropertyValue(TextElement.FontSizeProperty, fontSize);
+        }
+
+        public void SetAlignment(HorizontalAlignment alignment)
+        {
+            if (TextBoxContent != null)
+            {
+                Paragraph paragraph = TextBoxContent.CaretPosition.Paragraph;
+                if (paragraph != null)
+                {
+                    paragraph.TextAlignment = ConvertToTextAlignment(alignment);
+                }
+
+            }
+        }
+        private TextAlignment ConvertToTextAlignment(HorizontalAlignment alignment)
+        {
+            switch (alignment)
+            {
+                case HorizontalAlignment.Left:
+                    return TextAlignment.Left;
+                case HorizontalAlignment.Center:
+                    return TextAlignment.Center;
+                case HorizontalAlignment.Right:
+                    return TextAlignment.Right;
+                default:
+                    throw new ArgumentException("Непідтримуване значення HorizontalAlignment");
+            }
+        }
+    }
+}
---
+++ OOP/OOP Lesson 22/OOP Lesson 22/MainWindow.xaml.cs
@@ -0,0 +1,261 @@
+﻿using Microsoft.Win32;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_Lesson_22
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            cmbFontFamily.SelectionChanged += CmbFontFamily_SelectionChanged;
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+            cmbFontSize.SelectionChanged += CmbFontSize_SelectionChanged;
+        }
+
+        private void CmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                ComboBox comboBox = sender as ComboBox;
+                if (comboBox != null && comboBox.SelectedItem != null)
+                {
+                    string fontFamily = comboBox.SelectedItem.ToString();
+                    document.ApplyFontFamily(new FontFamily(fontFamily));
+                }
+            }
+        }
+
+        private void CmbFontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                ComboBox comboBox = sender as ComboBox;
+                if (comboBox != null && comboBox.SelectedItem != null)
+                {
+                    double fontSize = (double)comboBox.SelectedItem;
+                    document.ApplyFontSize(fontSize);
+                }
+            }
+        }
+
+        private int number = 1;
+        private void NewDocument_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem newTab = new TabItem();
+            newTab.Header = $"Документ {number}";
+            Document document = new Document(number);
+            newTab.Content = document;
+            TabControl.Items.Add(newTab);
+            TabControl.SelectedItem = newTab;
+            number++;
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                string fileText = System.IO.File.ReadAllText(filePath);
+
+                if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+                {
+                    activeDocument.SetText(fileText);
+                }
+                else
+                {
+                    MessageBox.Show("Немає активного документу всередині");
+                }
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.SaveDocument();
+            }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void CutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.Cut();
+            }
+        }
+
+        private void CopyMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.Copy();
+            }
+        }
+
+        private void PasteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.Paste();
+            }
+        }
+
+        private void SelectAllMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.SelectAll();
+            }
+        }
+
+        private void EnglishMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            fileMenu.Header = "File";
+            createMenuItem.Header = "Create";
+            openMenuItem.Header = "Open";
+            saveMenuItem.Header = "Save";
+            exitMenuItem.Header = "Exit";
+
+            editMenu.Header = "Edit";
+            copyMenuItem.Header = "Copy";
+            selectAllMenuItem.Header = "Select All";
+            pasteMenuItem.Header = "Paste";
+            cutMenuItem.Header = "Cut";
+            pasteimageMenuItem.Header = "Insert image";
+
+            languageMenu.Header = "Language";
+        }
+
+        private void PasteImage_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.InsertImage();
+            }
+        }
+
+        private void UkrainianMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+            fileMenu.Header = "Файл";
+            createMenuItem.Header = "Створити";
+            openMenuItem.Header = "Відкрити";
+            saveMenuItem.Header = "Зберегти";
+            exitMenuItem.Header = "Вийти";
+
+            editMenu.Header = "Редагувати";
+            copyMenuItem.Header = "Копіювати";
+            selectAllMenuItem.Header = "Виділити все";
+            pasteMenuItem.Header = "Вставити";
+            cutMenuItem.Header = "Вирізати";
+            pasteimageMenuItem.Header = "Вставити зображення";
+
+            languageMenu.Header = "Мова";
+        }
+
+        private void BoldButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                document.ApplyBold();
+            }
+        }
+
+        private void ItalicButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                document.ApplyItalic();
+            }
+        }
+
+        private void UnderlineButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                document.ApplyUnderline();
+            }
+        }
+
+        private void RemoveBold_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                document.RemoveBold();
+            }
+        }
+
+        private void RemoveItalic_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                document.RemoveItalic();
+            }
+        }
+
+        private void RemoveUnderline_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document document)
+            {
+                document.RemoveUnderline();
+            }
+        }
+
+        private void PasteImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.InsertImage();
+            }
+        }
+
+        private void LeftIcon_Click(object sender, RoutedEventArgs e)
+        {
+            SetAlignmentForActiveDocument(HorizontalAlignment.Left);
+        }
+
+        private void CenterIcon_Click(object sender, RoutedEventArgs e)
+        {
+            SetAlignmentForActiveDocument(HorizontalAlignment.Center);
+        }
+
+        private void RightIcon_Click(object sender, RoutedEventArgs e)
+        {
+            SetAlignmentForActiveDocument(HorizontalAlignment.Right);
+        }
+
+        private void SetAlignmentForActiveDocument(HorizontalAlignment alignment)
+        {
+            if (TabControl.SelectedItem is TabItem selectedTabItem && selectedTabItem.Content is Document activeDocument)
+            {
+                activeDocument.SetAlignment(alignment);
+            }
+        }
+    }
+}
